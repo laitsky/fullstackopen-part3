f445a89603aa8753ffbfdeb6c04c36ad68cf99bc
laitsky <vincent.diamond15@gmail.com>
2020-07-10 13:41:29 +0700

    3.3 done

--- index.js
+++ index.js
@@ -31,13 +31,24 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(note => note.id === id);
+
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+})
+
 app.get("/api/persons", (req, res) => {
   res.send(persons);
 });
 
 app.get("/info", (req, res) => {
   const personCount = persons.length;
-  
+
   res.send(`
     <p>Phonebook has info for ${personCount} people</p>
     <p>${new Date()}</p>
