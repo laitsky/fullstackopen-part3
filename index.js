e9eb74989ff095df2ac40b799357e51ca67cdf9f
laitsky <vincent.diamond15@gmail.com>
2020-07-10 13:31:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1,
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323532",
+    id: 2,
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3,
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+app.get("/info", (req, res) => {
+  const personCount = persons.length;
+  
+  res.send(`
+    <p>Phonebook has info for ${personCount} people</p>
+    <p>${new Date()}</p>
+    `);
+});
+PORT = 3001;
+app.listen(PORT, () => {});
